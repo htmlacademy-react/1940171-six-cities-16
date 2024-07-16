cbb078958473457794be60cdae9b498369b8eecb
Gleb Luzin <firecrow47@gmail.com>
2024-07-16 18:23:12 +0500

    2.16. Начинаем программировать

---
+++ src/components/app/app.tsx
@@ -0,0 +1,14 @@
+import MainPage from '../../pages/main/main-page';
+
+type AppScreenProps = {
+  cardCount: number;
+}
+
+
+function App({ cardCount }: AppScreenProps): JSX.Element {
+  return (
+    <MainPage cardCount={cardCount}/>
+  );
+}
+
+export default App;
